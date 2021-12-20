dc8435478671ae18ade0328e3d7af4a829dcd78a
Ali Koçman <34556282+Eddith@users.noreply.github.com>
2021-12-20 15:50:24 +0300

    prdoduct detail

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
 import "./App.scss";
 import Navbars from "./components/Navbars/Navbars";
 import Footer from "./components/Footer/Footer";
-import Home from "./screens/Home/Home";
+// import Home from "./screens/Home/Home";
+import ProductDetail from "./screens/ProductDetail/ProductDetail";
 
 function App() {
+
   return (
      <>
       <Navbars />
 
-      <Home />
+      <ProductDetail />
 
       <Footer />
     </>
--- src/screens/ProductDetail/ProductDetail.js
+++ src/screens/ProductDetail/ProductDetail.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import "./ProductDetail.scss"
+import { BsSuitHeart } from "react-icons/bs";
 
 function ProductDetail() {
   return (
     <>
-        <p>Product Detail</p>
+        <div className="app">
+            <div className="details" key="">
+              <div className="big-img">
+                <img src="https://wallpaperaccess.com/full/2960437.jpg" alt=""/>
+              </div>
+              <div className="box">
+                <div className="row">
+                  <h2>Urun Adı</h2>
+                  <span>Kategori</span>
+                </div>
+
+                <p>lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+                <p>€50</p>
+
+                <button className="cart"><BsSuitHeart /> Add to Favorites</button>
+
+              </div>
+            </div>
+      </div>
     </>
   );
 }
